7c8c0ca156657a9070e4110d0d1882a1b18da85a
Tracy Karanja <chirutracy@gmail.com>
2024-05-26 16:45:09 +0300

    new commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+# class -> Anagram, instance method -> match()
+# when given a word, it checks an array of words and returns it's anagram (a word that has the same letters as the word)
+# if there is none, it should return []
+
+class Anagram():
+    def __init__(self, word):
+        self.word = word
+        
+    def match(self, words_array):
+        return [word for word in words_array if sorted(word) == sorted(self.word)]
+
+# examples    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana'])) # should return ['inlets]
+
+word = Anagram("word")
+print(word.match(['hello', 'goodbye'])) # should return []
